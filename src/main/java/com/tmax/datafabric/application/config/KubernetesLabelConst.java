0c748ff90d071190b8e8a435027cc23d59104ddb
takhun_kim <takhun_kim@tmax.co.kr>
2024-10-23 12:04:34 +0900

    [Add] Analysis Workflow Watcher 추가

--- src/main/java/com/tmax/datafabric/application/config/KubernetesLabelConst.java
+++ src/main/java/com/tmax/datafabric/application/config/KubernetesLabelConst.java
@@ -1,5 +1,16 @@
 package com.tmax.datafabric.application.config;
 
+import java.util.Map;
+
 public class KubernetesLabelConst {
+    public static final String MODULE_KEY = "module";
+    public static final String WORKFLOW_TYPE_KEY = "workflow-type";
+
+    public static final String DATAFABRIC = "datafabric";
+
     public static final String ANALYSIS = "analysis";
+    public static final String ANALYSIS_ID_KEY = "analysis-id";
+
+    public static final Map<String, String> DEFAULT_ANALYSIS_LABELS = Map.of(MODULE_KEY, DATAFABRIC,
+        WORKFLOW_TYPE_KEY, ANALYSIS);
 }
---
+++ src/main/java/com/tmax/datafabric/application/watcher/WorkflowPhase.java
@@ -0,0 +1,8 @@
+package com.tmax.datafabric.application.watcher;
+
+public class WorkflowPhase {
+    public static final String PENDING = "pending";
+    public static final String RUNNING = "running";
+    public static final String SUCCEEDED = "succeeded";
+    public static final String FAILED = "failed";
+}
